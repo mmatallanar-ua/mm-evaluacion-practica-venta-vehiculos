2df75aad69811426e42edad99fc2eaff14b136c5
Miguel Angel Matallana Romero <m.matallanar@uniandes.edu.co>
2025-11-01 12:45:40 -0500

    Punto 2 - Se agregan los totales por marca

--- src/app/vehiculo/vehiculo.component.ts
+++ src/app/vehiculo/vehiculo.component.ts
@@ -10,18 +10,30 @@ import { VehiculoService } from './vehiculo.service';
 })
 export class VehiculoComponent implements OnInit {
   vehiculos: Vehiculo[] = [];
+  totalRenault = 0;
+  totalChevrolet = 0;
+  totalNissan = 0;
 
   constructor(private vehiculoService: VehiculoService) {}
 
   ngOnInit(): void {
     this.getVehiculos();
+    
+
   }
 
   getVehiculos(): void {
     this.vehiculoService.getVehiculos().subscribe(data => {
       console.log(data);
       this.vehiculos = data;
+      this.getTotalMarca();
     });
+  }
+
+  getTotalMarca(): void {
+    this.totalRenault = this.vehiculos.filter(v => v.marca === 'Renault').length;
+    this.totalChevrolet = this.vehiculos.filter(v => v.marca === 'Chevrolet').length;
+    this.totalNissan = this.vehiculos.filter(v => v.marca === 'Nissan').length;
   }
 
 
