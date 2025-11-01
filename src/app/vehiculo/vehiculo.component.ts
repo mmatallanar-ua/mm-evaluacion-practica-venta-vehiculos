8ad997b4034791885ea2cadfaf2708ef7bd2b4ad
Miguel Angel Matallana Romero <m.matallanar@uniandes.edu.co>
2025-11-01 10:42:42 -0500

    Se crea la estructura del modelo

---
+++ src/app/vehiculo/vehiculo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-vehiculo',
+  templateUrl: './vehiculo.component.html',
+  styleUrls: ['./vehiculo.component.css'],
+  standalone: false
+})
+export class VehiculoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
